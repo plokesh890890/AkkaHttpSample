4cd2823b5c3c6c1cdc7526720a5f7f752af9823a
Lokesh <plokesh89890@gmail.com>
2022-02-10 18:32:47 +0530

    Fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting server..."
 cd /home/ec2-user/app1/scripts
-java -jar ../AkkatHttpTest-1.0-SNAPSHOT.jar
+java -jar ../AkkatHttpTest-1.0-SNAPSHOT.jar &
 echo "DEBUG: after java command"
 exit 0
