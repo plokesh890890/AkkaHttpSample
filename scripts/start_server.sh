5b1ac0a9a2eec21251d8daac14b47d9f38e91299
Lokesh <plokesh89890@gmail.com>
2022-02-10 12:58:09 +0530

    Change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,4 @@
 echo "Starting server..."
 cd /home/ec2-user/app1/scripts
 java -jar ../AkkatHttpTest-1.0-SNAPSHOT.jar
-
 exit 0
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,4 +3,5 @@ echo "Killing the app running on 8081"
 PID=$(sudo lsof -t -i:8081)
 if [ $PID ]; then
 	kill -9 $(sudo lsof -t -i:8081)
+fi
 exit 0
